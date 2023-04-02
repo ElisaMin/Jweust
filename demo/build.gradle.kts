6da85a1c352bb61372343958c5c50df65109ec08
Heizi <xm.zhongjiaxin@gmail.com>
2023-04-02 20:57:19 +0800

    init demo

---
+++ demo/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.kotlin)
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,4 @@
 
 rootProject.name = "Jweust"
 include("plugin")
+include("demo")
