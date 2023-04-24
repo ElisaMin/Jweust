e5ba9f10d0fe053fa0324304221546133f92eb9b
Heizi <xm.zhongjiaxin@gmail.com>
2023-04-24 14:26:13 +0800

    update script to usable

--- compose-demo/build.gradle.kts
+++ compose-demo/build.gradle.kts
@@ -1,25 +1,38 @@
+@file:Suppress("unstableApiUsage")
+
 plugins {
     alias(libs.plugins.kotlin)
     alias(libs.plugins.compose)
+    alias(libs.plugins.jweust)
 }
 dependencies {
     implementation(compose.desktop.currentOs)
+    implementation(libs.khell.self)
 }
 repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    `use heizi's maven repo by github`()
 }
 
 group = "me.heizi.jweust.demo.compose"
 version = "1.0.0"
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
 
 kotlin {
-    jvmToolchain(19)
     target { compilations.all {
         kotlinOptions {
             freeCompilerArgs += "-Xcontext-receivers"
         }
     } }
+}
+
+jweust {
+    defaults()
 }
 
 compose.desktop {
