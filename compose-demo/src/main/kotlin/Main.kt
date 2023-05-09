df61e5e5c8830f76b6c4e09def5ae5dcddaab643
Heizi <xm.zhongjiaxin@gmail.com>
2023-05-10 06:35:44 +0800

    error for exit checking

--- compose-demo/src/main/kotlin/Main.kt
+++ compose-demo/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -16,10 +17,17 @@ fun App() = Box(Modifier.fillMaxSize()) {
     var text by remember { mutableStateOf("Hello, Jweust!") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }, modifier = Modifier.align(Alignment.Center)) {
-            Text(text)
+        Row(modifier = Modifier.align(Alignment.Center)) {
+            Button(content = {
+                Text(text)
+            }, onClick = {
+                text = "Hello, Desktop!"
+            })
+            Button(content = {
+                Text("ERROR")
+            }, onClick = {
+                throw IllegalStateException("ERROR")
+            })
         }
     }
 }
