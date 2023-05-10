d92d8edf0b87853b69e6c1ce09f1a6311ca53ab9
Heizi <xm.zhongjiaxin@gmail.com>
2023-05-11 05:32:32 +0800

    update: done test and run

--- plugin/src/functionalTest/kotlin/me/heizi/jweust/JweustPluginFunctionalTest.kt
+++ plugin/src/functionalTest/kotlin/me/heizi/jweust/JweustPluginFunctionalTest.kt
@@ -16,11 +16,11 @@ class JweustPluginFunctionalTest {
     @field:TempDir
     lateinit var projectDir: File
 
-    private val buildFile by lazy { projectDir.resolve("build.gradle.kts") }
+    private val buildFile by lazy {
+        projectDir.resolve("build.gradle.kts")
+    }
     private val settingsFile by lazy { projectDir.resolve("settings.gradle.kts") }
-
-    @Test fun `can run task`() {
-        // Set up the test build
+    private fun initProject() {
         settingsFile.writeText("")
         buildFile.writeText("""
             plugins {
@@ -32,10 +32,33 @@ class JweustPluginFunctionalTest {
             jweust {
                 defaults()
                 includeJarByGenerate()
+                jar {
+                    mainClass = "Main"
+                }
             }
         """.trimIndent())
+        projectDir.resolve("src/main/java/")
+            .also { it.mkdirs() }
+            .resolve("Main.java").
+        writeText(
+            """
+            |public class Main {
+            |    public static void main(String[] args) {
+            |        System.out.println("Hello World!");
+            |        try {
+            |            System.out.println("Sleeping 5s");
+            |            Thread.sleep(5000);
+            |        } catch (InterruptedException e) {
+            |            e.printStackTrace();
+            |        }
+            |    }
+            |}
+        """.trimMargin())
+    }
 
-
+    @Test fun `can run task`() {
+        // Set up the test build
+        initProject()
         // Run the build
         GradleRunner.create().
         withPluginClasspath().
@@ -43,9 +66,23 @@ class JweustPluginFunctionalTest {
         withProjectDir(projectDir).
         forwardOutput().
         build()
-//
+        val ext = projectDir.resolve("build/jweust/").listFiles()!!.find { it.name.endsWith(".exe") }!!
+        println(ext)
+        // println size of exe in mb
+        println("exe.size ="+ ext.length().toDouble() / 1024 + "kb")
+        val jar = projectDir.resolve("build/libs/").listFiles()!!.find { it.name.endsWith(".jar") }!!
+        println("jar.size ="+ jar.length().toDouble() / 1024 + "kb")
+        println("size.dst ="+ (ext.length()-jar.length()).toDouble() / 1024 + "kb")
 
-//        // Verify the result
-//        assertTrue(result.output.contains("Hello from plugin 'me.heizi.jweust.greeting'"))
+
+        // Run the exe
+        println("Running exe, it will take 5s cuzing by Thread.sleep(5000) ")
+        ProcessBuilder(ext.absolutePath)
+            .start().run {
+                val r = inputStream.bufferedReader().readText()
+                waitFor()
+                println(r)
+                assert(r.contains("Hello World!") && r.contains("Sleeping 5s"))
+            }
     }
 }
